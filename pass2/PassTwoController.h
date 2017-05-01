5888caf20e56b5d9e6ea0bf23a2c08ad71a5e4bb
Salma-Ahmed <salma19956@gmail.com>
2017-05-01 19:18:08 +0200

    configured objectCodeWriter

--- pass2/PassTwoController.h
+++ pass2/PassTwoController.h
@@ -1,12 +1,30 @@
 //
-// Created by amor2 on 5/1/2017.
+// Created by SalmaAhmed on 5/1/2017.
 //
 
 #ifndef SIC_ASSEMBLER_PASSTWOCONTROLLER_H
 #define SIC_ASSEMBLER_PASSTWOCONTROLLER_H
 
 
+#include <string>
+#include "ObjectCodeWriter.h"
+
+using namespace std;
 class PassTwoController {
+public:
+    PassTwoController(string intermediateFile,string programLength,string objectFile);
+    void executePass2();
+
+private:
+    ObjectCodeWriter *objectWriter;
+    string intermediateFile;
+    string outputFile;
+    string programLength;
+
+
+
+
+
 
 };
 
