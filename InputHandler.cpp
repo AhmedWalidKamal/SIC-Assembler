7bdde96c255f17516dc4306224f4a1180aa68265
Omar Walid <ahmed3walid96@gmail.com>
2017-05-03 16:21:25 +0200

    Modified Input handler.

--- InputHandler.cpp
+++ InputHandler.cpp
@@ -2,59 +2,37 @@
 #include <fstream>
 #include <iostream>
 #include <vector>
-
-//std::vector<std::string> readFileToVector(const std::string& filename)
-//{
-//    std::ifstream source;
-//    source.open(filename);
-//    std::vector<std::string> lines;
-//    std::string line;
-//    while (std::getline(source, line))
-//    {
-//        lines.push_back(line);
-//    }
-//    return lines;
-//}
-//
-//void displayVector(const std::vector<std::string> v)
-//{
-//    for (int i(0); i != v.size(); ++i)
-//        std::cout << "\n" << v[i];
-//}
+#include "MainController.h"
 
 /// Input Handler is responsible for handling the input commands, like if we wanted to add -help or
 /// When the user wants to use -sic or -sicXE, then it passes the source file name to the main controller
 /// then the main controller constructs a file reader object with the source file and passes this object to pass1
 /// also the main controller constructs a file writer object with the object file and passes it to pass2
 
-int handleOptionsArgs();
-
-void readFile(char *string);
+int handleOptionsArgs(char **pString);
+void readFile(std::string fileName);
 
 int main(int argc, char **argv) {
     // argv contains possible -commands or file name directly, pass it to main controller and start working.
     if (argc > 1) {
-        int fileNameIndex = handleOptionsArgs();
+        int fileNameIndex = handleOptionsArgs(argv);
         readFile(argv[fileNameIndex]);
-//        std::string charactersFilename(argv[1]);
-//        std::vector<std::string> characters = readFileToVector(charactersFilename);
-//
-//        displayVector(characters);
     } else {
-        std::cout << "No filename was entered.";
+        std::cout << "No file name was entered.";
     }
     return 0;
 }
 
 // This function sends the file name to the main controller and calls execute
-void readFile(char *string) {
-
+void readFile(std::string fileName) {
+    MainController *mainController = new MainController();
+    mainController->execute(fileName);
 }
 
-// This function is resposible for handling the command line arguments like -sic, -sicXE, -help or other stuff
-// and it returns the index of the file name
+// This function is responsible for handling the command line arguments like -sic, -sicXE, -help or other stuff
+// and it returns the index of the file name or exits program if user wanted help manual or something.
 /// TODO: Implement this.
-int handleOptionsArgs() {
+int handleOptionsArgs(char **arguments) {
     int fileNameIndex = 1;
     return fileNameIndex;
 }
