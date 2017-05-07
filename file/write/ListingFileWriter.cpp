c3346aebb8c6760a1656fb8594de0426cd36cbbc
Omar Walid <ahmed3walid96@gmail.com>
2017-05-07 15:46:50 +0200

    Replaced \n with endl.

--- file/write/ListingFileWriter.cpp
+++ file/write/ListingFileWriter.cpp
@@ -15,8 +15,8 @@ ListingFileWriter::ListingFileWriter(const std::string &fileName)
 void ListingFileWriter::writeInitialLine() {
 
     listFileStream << stringUtil->fillSpaces("Line", LINE_FORMAT) << stringUtil->fillSpaces("Loc", LOC_FORAMT);
-    listFileStream << stringUtil->fillSpaces("Source Statement", STATEMENT_FORAMT) << "Object Code" << "\n";
-    listFileStream << stringUtil->drawLine(LINE_LENGTH) << "\n";
+    listFileStream << stringUtil->fillSpaces("Source Statement", STATEMENT_FORAMT) << "Object Code" << std::endl;
+    listFileStream << stringUtil->drawLine(LINE_LENGTH) << std::endl;
 }
 
 void ListingFileWriter::writeLine(int lineNumber, Statement *statement, std::string objectCode) {
@@ -37,17 +37,17 @@ void ListingFileWriter::writeLine(int lineNumber, Statement *statement, std::str
     if (objectCode.length() > 0)
         objectCode = stringUtil->fillZeros(objectCode, 6);
 
-    listFileStream << lineNum << locationCounter << label << mnemonic << operand << comment << objectCode << "\n";;
+    listFileStream << lineNum << locationCounter << label << mnemonic << operand << comment << objectCode << std::endl;;
 
 }
 
 void ListingFileWriter::writeError(ErrorHandler::Error error) {
     listFileStream << "ERROR: " ;
-    listFileStream<< ErrorHandler::errors[error] << "\n";
+    listFileStream<< ErrorHandler::errors[error] << std::endl;
 }
 
 void ListingFileWriter::writeComment(int lineNumber, std::string line) {
     // Write comment line starting from source statement after writing line number (no loc ctr is written here i guess).
     std::string lineNum = stringUtil->fillSpaces(stringUtil->toString(lineNumber), SPACE_BOUND);
-    listFileStream<<lineNum<<line<<"\n";
+    listFileStream<<lineNum<<line<<std::endl;
 }
