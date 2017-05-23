7d1bc7170a171a5eb769a0b0a8a5ea031d6bca26
Shrouk_Ashraf <shroukashraf8@gmail.com>
2017-05-23 16:36:28 +0200

    added hexvalue arrtibute

--- statement/Operand.h
+++ statement/Operand.h
@@ -23,6 +23,7 @@ public:
     bool isIndexed();
     const std::string &getOperandField() const;
     const std::string &getrawInput() const;
+    const std::string &getHexValue() const;
     void setOperandField(const std::string &operandField);
     int getLCIncrement() const;
     void setOperandValue(int i);
@@ -36,17 +37,18 @@ public:
     bool validateHexConstant();
     bool validateDecimalValue();
 private:
-    std::string operandField;
+    std:: string operandField;
     std:: string rawInput;
+    std:: string hexValue;
     enum OperandType {Label,hexaAddress,decimalAddress,
         currentLocationCounter,StringConstant,
-        HexConstant,DecimalValue,inValid, hexConstLiteral,
-        stringConstLiteral, decimalLiteral};
+        HexConstant,DecimalValue,inValid};
     OperandType type = inValid;
     int operandValue = 0;
     int LCIncrement = 0;
-    bool indexed = false;
 
+    bool indexed = false;
+    bool literal = false;
     void validateLiteral();
 };
 
